eb694fc7e840c7138b076658a9f6c372a51707c2
yamadanatsuko0720 <makinonatsuko@yahoo.co.jp>
2020-10-14 16:54:48 +0900

    ログアウト、ログイン機能、パスワードバリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])\w{6,100}\z/
+  validates :password, presence: true,
+            format: { with: VALID_PASSWORD_REGEX}
+
 end
