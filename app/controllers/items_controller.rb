7bd0a83b99c27e816e4ccdc137125015bf7ad450
yamadanatsuko0720 <makinonatsuko@yahoo.co.jp>
2020-10-15 11:45:01 +0900

    出品ページに遷移、ActiveHashのモデル作成、Itemモデル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,20 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
   private
+
+  def item_params
+    params.require(:item).permit(:image, :text).merge(user_id: current_user.id)
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
