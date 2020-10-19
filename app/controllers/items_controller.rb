04ef9da044a28b13042af14fab6758684f7a40c6
yamadanatsuko0720 <makinonatsuko@yahoo.co.jp>
2020-10-19 18:37:29 +0900

    指摘内容の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:create, :show, :edit, :update]
   before_action :authenticate_user!, except: [:index, :new, :show]
   before_action :move_to_index, only: [:edit]
 
@@ -14,7 +15,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -23,15 +23,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    item = Item.find(params[:id])
     if item.update(item_params)
       redirect_to item_path(item.id)
     else
@@ -40,6 +37,10 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :postage_payer_id, :prefecture_id, :preparation_day_id, :price).merge(user_id: current_user.id)
