5fd1347dc0f6dc406ef5463c4c13235c8e9b8485
yamadanatsuko0720 <makinonatsuko@yahoo.co.jp>
2020-10-19 20:21:04 +0900

    指摘内容修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :destroy]
   before_action :move_to_index, only: [:edit]
 
   def index
