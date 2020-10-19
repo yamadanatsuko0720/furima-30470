4a467ef0ee2a5126de619c65e60a5f26c6f9dae4
yamadanatsuko0720 <makinonatsuko@yahoo.co.jp>
2020-10-19 19:39:20 +0900

    不具合箇所修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:create, :show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit]
 
@@ -15,6 +15,7 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
