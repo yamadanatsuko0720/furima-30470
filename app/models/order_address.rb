c4080748841890bcf0fded3b3f49bbe646cf9bfe
yamadanatsuko0720 <makinonatsuko@yahoo.co.jp>
2020-10-21 14:39:05 +0900

    soldout表示の実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -12,7 +12,6 @@ class OrderAddress
 
 
   def save
-    # binding.pry
     order = Order.create(item_id: item_id, user_id: user_id)
     Address.create!(post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name,phone_number: phone_number, order_id: order.id)
   end
