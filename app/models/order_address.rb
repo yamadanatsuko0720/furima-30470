98418bb8b75d97435f708ab3b869db11fb74e7f1
yamadanatsuko0720 <makinonatsuko@yahoo.co.jp>
2020-10-21 17:55:05 +0900

    指摘内容修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,12 +5,11 @@ class OrderAddress
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
+    validates :token
     validates :house_number
     validates :phone_number, format: { with: /\A\d{11}\z/, message: "is invalid. Input half-width characters."}
   end
   validates :prefecture_id, numericality: { other_than: 1 } 
-  validates :token, presence: true
-
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :order_address do
     token {"tok_abcdefghijk00000000000000000"}
     post_code {"123-4567"}
-    prefecture_id {"2"}
+    prefecture_id {2}
     city {"目黒区"}
     house_number {"1-1-1"}
     building_name{"てすと"}
