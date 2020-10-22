8a863d1dc190d7669fc0e3e2f2ba88f9f68d5d08
yamadanatsuko0720 <makinonatsuko@yahoo.co.jp>
2020-10-22 11:24:59 +0900

    configure_permitted_parameters追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
+  end
 end
