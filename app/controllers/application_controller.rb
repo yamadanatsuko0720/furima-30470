3c4ba6f809abc82ed0a737fe7ca766315f3ad5dd
yamadanatsuko0720 <makinonatsuko@yahoo.co.jp>
2020-10-22 11:13:30 +0900

    basic認証修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,7 @@ class ApplicationController < ActionController::Base
   private
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
-
-  def configure_permitted_parameters
-    username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  end
-
 end
